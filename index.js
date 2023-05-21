8153af42e54da1425af46782bfd4cb333e188fc8
thach vu <vuhoangthach1402@gmail.com>
2023-05-21 09:50:15 +0700

    .

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const qrcode = require('qrcode');
 
 const app = express();
 const port = 3000;
-
+app.get('/',(req,res)=>{
+  res.send('welcome api barcode generator')
+})
 // Định nghĩa API endpoint
 app.get('/barcode', (req, res) => {
   const url = req.query.url; // Lấy URL từ tham số truy vấn
